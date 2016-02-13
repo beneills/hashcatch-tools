d5d04eaa6b9556dc43b7f01219e7a45d9f21dcde
Ben Eills <ben@beneills.com>
2016-02-13 16:14:23 +0000

    Remove (deleted) Amazon key from repo

--- amazon.rb
+++ amazon.rb
@@ -2,10 +2,6 @@ require 'amazon/ecs'
 
 require "#{File.dirname(__FILE__)}/check_rails.rb"
 
-
-# ben@beneills.com secrets
 Amazon::Ecs.options = {
-  :associate_tag => 'hash0e-21',
-  :AWS_access_key_id => 'AKIAJ3VAUXSMBNNFMEUQ',
-  :AWS_secret_key => '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',
+  # blah
 }
